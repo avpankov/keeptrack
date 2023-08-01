6b1a50c32f0a4f0eb91f8274f63dfeb5431a295f
Панков Артем Владимирович <pankov_AV@GOIK.LOCAL>
2023-08-01 15:30:15 +0300

    feat: add routes

---
+++ src/projects/ProjectPage.tsx
@@ -0,0 +1,62 @@
+import React, { useEffect, useState } from 'react';
+import { projectAPI } from './projectAPI';
+import ProjectDetail from './ProjectDetail';
+import { Project } from './Project';
+import { useParams } from 'react-router-dom';
+
+function ProjectPage() {
+  const [loading, setLoading] = useState(false);
+  const [project, setProject] = useState<Project | null>(null);
+  const [error, setError] = useState<string | null>(null);
+  const params = useParams();
+  const id = Number(params.id);
+
+  useEffect(() => {
+    async function loadProject() {
+      setLoading(true);
+      try {
+        const data = await projectAPI.find(id);
+        setProject(data);
+      } catch (e) {
+        if (e instanceof Error) {
+          setError(e.message);
+          setLoading(false);
+        }
+      } finally {
+        setLoading(false);
+      }
+    }
+    loadProject();
+  }, [id]);
+
+  return (
+    <div>
+      <>
+        <h1>Project Detail</h1>
+
+        {loading && (
+          <div className="center-page">
+            <span className="spinner primary"></span>
+            <p>Loading...</p>
+          </div>
+        )}
+
+        {error && (
+          <div className="row">
+            <div className="card large error">
+              <section>
+                <p>
+                  <span className="icon-alert inverse "></span> {error}
+                </p>
+              </section>
+            </div>
+          </div>
+        )}
+
+        {project && <ProjectDetail project={project} />}
+      </>
+    </div>
+  );
+}
+
+export default ProjectPage;
